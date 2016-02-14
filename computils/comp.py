4768ab40ddef462b1011078421b45f19ee8cf522
Paulus Esterhazy <pesterhazy@gmail.com>
2016-02-14 17:48:55 +0100

    Add slurping without nextp

--- computils/comp.py
+++ computils/comp.py
@@ -5,22 +5,39 @@ from subprocess import Popen, PIPE
 
 ACTIONS = ["slurp"]
 
+def split_list(lst, e, maxi):
+    n = 0
+    chunks = []
+    cur = []
+    while True:
+        if not lst:
+            chunks.append(cur)
+            break
+
+        if lst[0] == e and n < maxi:
+            chunks.append(cur)
+            n += 1
+            cur = []
+        else:
+            cur.append(lst[0])
+
+        lst = lst[1:]
+
+    return chunks
+
+
 def slurp(*args):
-    try:
-        idx = args.index("--")
-        before = args[:idx]
-        after = args[(idx+1):]
-    except ValueError:
-        before = args
-        after = ()
+    chunks = split_list(args, "--", 1)
 
-    assert len(after) > 0, "Need nextp"
-
-    cat = Popen(["/bin/cat"] + list(before), stdout=PIPE)
-    nextp = Popen(after, stdin=cat.stdout)
-    cat.stdout.close()
-    cat.wait()
-    nextp.wait()
+    if len(chunks) > 1:
+        cat = Popen(["/bin/cat"] + chunks[0], stdout=PIPE)
+        nextp = Popen(chunks[1], stdin=cat.stdout)
+        cat.stdout.close()
+        cat.wait()
+        nextp.wait()
+    else:
+        cat = Popen(["/bin/cat"] + chunks[0])
+        cat.wait()
 
 def run(*args):
     assert len(args)>1
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,9 +4,28 @@ from .context import comp
 
 sample = "foo\n"
 
-def test_comp(tmpdir, capfd):
+def test_slurp(tmpdir, capfd):
     f = tmpdir.join("input")
     f.write(sample)
     comp.run("slurp", str(f), "--", "cat")
 
     assert sample == capfd.readouterr()[0]
+
+def test_slurp2(tmpdir, capfd):
+    f = tmpdir.join("input")
+    f.write(sample)
+    comp.run("slurp", str(f))
+
+    assert sample == capfd.readouterr()[0]
+
+def test_split_list():
+    ex = ["a", "b", "a", "b", "a"]
+
+    assert [ex] == comp.split_list(ex, "b", 0)
+    assert [["a"], ["a","b","a"]] == comp.split_list(ex, "b", 1)
+    assert [["a"], ["a"],["a"]] == comp.split_list(ex, "b", 2)
+
+
+    assert [[]] == comp.split_list([], "b", 2)
+    assert [[],[]] == comp.split_list(["b"], "b", 2)
+    assert [[],["a"]] == comp.split_list(["b","a"], "b", 2)
